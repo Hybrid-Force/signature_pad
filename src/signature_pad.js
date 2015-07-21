6c28b0b133e8d7319c54afba952c3f8424c60df7
Derek Hsu <derekhybrid@gmail.com>
2015-07-21 18:04:35 +0800

    Update control point calculation algorithm for cubic bezier curve. Add option to use liner, quadratic or cubic bezier curve. Monir performance improvements. Bump up version.

--- src/signature_pad.js
+++ src/signature_pad.js
@@ -8,6 +8,12 @@ var SignaturePad = (function (document) {
         this.velocityFilterWeight = opts.velocityFilterWeight || 0.7;
         this.minWidth = opts.minWidth || 0.5;
         this.maxWidth = opts.maxWidth || 2.5;
+        this.cpTension = opts.cpTension || 0.5;
+        this.curveOrder = opts.curveOrder || 3;
+        if (this.curveOrder <2 || this.curveOrder > 4) {
+            throw Error('Invalid curveOrder'+this.curveOrder);
+        }
+        this.throttle = opts.throttle || 16;
         this.dotSize = opts.dotSize || function () {
             return (this.minWidth + this.maxWidth) / 2;
         };
@@ -18,6 +24,8 @@ var SignaturePad = (function (document) {
 
         this._canvas = canvas;
         this._ctx = canvas.getContext("2d");
+        this._ctx.lineCap = 'round';
+        this._ctx.lineJoin = 'round';
         this.clear();
         this._canvasWidthScale = null;
         this._canvasHeightScale = null;
@@ -173,6 +181,7 @@ var SignaturePad = (function (document) {
         this._lastWidth = (this.minWidth + this.maxWidth) / 2;
         this._isEmpty = true;
         this._ctx.fillStyle = this.penColor;
+        this._ctx.strokeStyle = this.penColor;
     };
 
     SignaturePad.prototype._getCanvasWidthScale = function () {
@@ -197,53 +206,65 @@ var SignaturePad = (function (document) {
         );
     };
 
+    SignaturePad.prototype._throttle = function (point) {
+        var lastPoint = this.points[this.points.length-1];
+        if (lastPoint &&
+            lastPoint.sqrDistanceTo(point) < this.throttle) {
+            return false;
+        } else {
+            return true;
+        }
+    };
+
     SignaturePad.prototype._addPoint = function (point) {
         var points = this.points,
             c2, c3,
             curve, tmp;
 
+        if (!this._throttle(point)) {
+            return;
+        }
+
         points.push(point);
 
-        if (points.length > 2) {
-            // To reduce the initial lag make it work with 3 points
-            // by copying the first point to the beginning.
-            if (points.length === 3) points.unshift(points[0]);
+        // To reduce the initial lag make it work with fewer points
+        // by copying the first point to the beginning.
+        while (points.length < this.curveOrder) {
+            points.unshift(points[0]);
+        }
 
+        if (this.curveOrder === 2) {
+            curve = new Bezier(points[0], undefined, undefined, points[1]);
+        } else if (this.curveOrder === 3) {
+            c2 = new Point((points[0].x + points[1].x)/2.0, (points[0].y+points[1].y)/2.0);
+            c3 = new Point((points[1].x + points[2].x)/2.0, (points[1].y+points[2].y)/2.0);
+            curve = new Bezier(c2, points[1], undefined, c3);
+        } else if (this.curveOrder === 4) {
             tmp = this._calculateCurveControlPoints(points[0], points[1], points[2]);
             c2 = tmp.c2;
             tmp = this._calculateCurveControlPoints(points[1], points[2], points[3]);
             c3 = tmp.c1;
             curve = new Bezier(points[1], c2, c3, points[2]);
-            this._addCurve(curve);
-
-            // Remove the first element from the list,
-            // so that we always have no more than 4 points in points array.
-            points.shift();
         }
+
+
+        this._addCurve(curve);
+
+        // Remove the first element from the list,
+        // so that we always have no more than `this.curveOrder` points in points array.
+        points.shift();
     };
 
     SignaturePad.prototype._calculateCurveControlPoints = function (s1, s2, s3) {
-        var dx1 = s1.x - s2.x, dy1 = s1.y - s2.y,
-            dx2 = s2.x - s3.x, dy2 = s2.y - s3.y,
-
-            m1 = {x: (s1.x + s2.x) / 2.0, y: (s1.y + s2.y) / 2.0},
-            m2 = {x: (s2.x + s3.x) / 2.0, y: (s2.y + s3.y) / 2.0},
-
-            l1 = Math.sqrt(dx1*dx1 + dy1*dy1),
-            l2 = Math.sqrt(dx2*dx2 + dy2*dy2),
-
-            dxm = (m1.x - m2.x),
-            dym = (m1.y - m2.y),
-
-            k = l2 / (l1 + l2),
-            cm = {x: m2.x + dxm*k, y: m2.y + dym*k},
-
-            tx = s2.x - cm.x,
-            ty = s2.y - cm.y;
+        var t = 0.5;
+        var d01 = s1.distanceTo(s2);
+        var d12 = s2.distanceTo(s3);
+        var fa=t*d01/(d01+d12);   // scaling factor for triangle Ta
+        var fb=t*d12/(d01+d12);   // ditto for Tb, simplifies to fb=t-fa
 
         return {
-            c1: new Point(m1.x + tx, m1.y + ty),
-            c2: new Point(m2.x + tx, m2.y + ty)
+            c1: new Point(s2.x-fa*(s3.x-s1.x), s2.y-fa*(s3.y-s1.y)),
+            c2: new Point(s2.x+fb*(s3.x-s1.x), s2.y+fb*(s3.y-s1.y))
         };
     };
 
@@ -252,56 +273,45 @@ var SignaturePad = (function (document) {
             endPoint = curve.endPoint,
             velocity, newWidth;
 
-        velocity = endPoint.velocityFrom(startPoint);
-        velocity = this.velocityFilterWeight * velocity
-            + (1 - this.velocityFilterWeight) * this._lastVelocity;
+        if (this.maxWidth !== this.minWidth) {
+            velocity = endPoint.velocityFrom(startPoint);
+            velocity = this.velocityFilterWeight * velocity
+                + (1 - this.velocityFilterWeight) * this._lastVelocity;
 
-        newWidth = this._strokeWidth(velocity);
-        this._drawCurve(curve, this._lastWidth, newWidth);
+            newWidth = this._strokeWidth(velocity);
+            this._drawCurve(curve, this._lastWidth, newWidth);
 
-        this._lastVelocity = velocity;
-        this._lastWidth = newWidth;
+            this._lastVelocity = velocity;
+            this._lastWidth = newWidth;
+        } else {
+            this._drawCurve(curve, this._lastWidth, this._lastWidth);
+        }
     };
 
     SignaturePad.prototype._drawPoint = function (x, y, size) {
         var ctx = this._ctx;
 
         ctx.moveTo(x, y);
-        ctx.arc(x, y, size, 0, 2 * Math.PI, false);
+        ctx.arc(x, y, size/2.0, 0, 2 * Math.PI, false);
         this._isEmpty = false;
     };
 
     SignaturePad.prototype._drawCurve = function (curve, startWidth, endWidth) {
-        var ctx = this._ctx,
-            widthDelta = endWidth - startWidth,
-            drawSteps, width, i, t, tt, ttt, u, uu, uuu, x, y;
+        var ctx = this._ctx;
 
-        drawSteps = Math.floor(curve.length());
         ctx.beginPath();
-        for (i = 0; i < drawSteps; i++) {
-            // Calculate the Bezier (x, y) coordinate for this step.
-            t = i / drawSteps;
-            tt = t * t;
-            ttt = tt * t;
-            u = 1 - t;
-            uu = u * u;
-            uuu = uu * u;
 
-            x = uuu * curve.startPoint.x;
-            x += 3 * uu * t * curve.control1.x;
-            x += 3 * u * tt * curve.control2.x;
-            x += ttt * curve.endPoint.x;
-
-            y = uuu * curve.startPoint.y;
-            y += 3 * uu * t * curve.control1.y;
-            y += 3 * u * tt * curve.control2.y;
-            y += ttt * curve.endPoint.y;
-
-            width = startWidth + ttt * widthDelta;
-            this._drawPoint(x, y, width);
+        ctx.moveTo(curve.startPoint.x, curve.startPoint.y);
+        if (curve.control1 && curve.control2) {
+            ctx.bezierCurveTo(curve.control1.x, curve.control1.y, curve.control2.x, curve.control2.y, curve.endPoint.x, curve.endPoint.y);
+        } else if (curve.control1) {
+            ctx.quadraticCurveTo(curve.control1.x, curve.control1.y, curve.endPoint.x, curve.endPoint.y);
+        } else {
+            ctx.lineTo(curve.endPoint.x, curve.endPoint.y);
         }
-        ctx.closePath();
-        ctx.fill();
+
+        ctx.lineWidth = endWidth;
+        ctx.stroke();
     };
 
     SignaturePad.prototype._strokeWidth = function (velocity) {
@@ -320,7 +330,11 @@ var SignaturePad = (function (document) {
     };
 
     Point.prototype.distanceTo = function (start) {
-        return Math.sqrt(Math.pow(this.x - start.x, 2) + Math.pow(this.y - start.y, 2));
+        return Math.sqrt(this.sqrDistanceTo(start));
+    };
+
+    Point.prototype.sqrDistanceTo = function (start) {
+        return Math.pow(this.x - start.x, 2) + Math.pow(this.y - start.y, 2);
     };
 
     var Bezier = function (startPoint, control1, control2, endPoint) {
@@ -328,34 +342,6 @@ var SignaturePad = (function (document) {
         this.control1 = control1;
         this.control2 = control2;
         this.endPoint = endPoint;
-    };
-
-    // Returns approximated length.
-    Bezier.prototype.length = function () {
-        var steps = 10,
-            length = 0,
-            i, t, cx, cy, px, py, xdiff, ydiff;
-
-        for (i = 0; i <= steps; i++) {
-            t = i / steps;
-            cx = this._point(t, this.startPoint.x, this.control1.x, this.control2.x, this.endPoint.x);
-            cy = this._point(t, this.startPoint.y, this.control1.y, this.control2.y, this.endPoint.y);
-            if (i > 0) {
-                xdiff = cx - px;
-                ydiff = cy - py;
-                length += Math.sqrt(xdiff * xdiff + ydiff * ydiff);
-            }
-            px = cx;
-            py = cy;
-        }
-        return length;
-    };
-
-    Bezier.prototype._point = function (t, start, c1, c2, end) {
-        return          start * (1.0 - t) * (1.0 - t)  * (1.0 - t)
-               + 3.0 *  c1    * (1.0 - t) * (1.0 - t)  * t
-               + 3.0 *  c2    * (1.0 - t) * t          * t
-               +        end   * t         * t          * t;
     };
 
     return SignaturePad;
